cb9c84f6de9200cb0ffa263cfc1ca8b176f33dbd
RAMENSKII <RA>
2021-11-01 18:29:01 +0700

    Initial Commit

---
+++ TrafficLightMVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLightMVP
+//
+//  Created by Kirill on 01.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
